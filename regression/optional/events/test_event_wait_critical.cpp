a5dc1ec4df32ec2309dd173fb2eefa175c9fc752
Mellor-Crummey <johnmc@login1.po.rice.edu>
2015-01-03 17:36:07 -0600

    flesh out test of event wait critical.

--- regression/optional/events/test_event_wait_critical.cpp
+++ regression/optional/events/test_event_wait_critical.cpp
@@ -4,65 +4,64 @@
 #include <assert.h>
 #include <common.h>
 
-//using namespace std;
-//long thread_base_index = 0;
-//map<long, OMPTAutomatonState *> threadId_to_state;
-//pthread_mutex_t map_mutex;
-//
-//void on_wait_critical(ompt_wait_id_t wait_id)
-//{
-//    //long thread_index = fetch_and_increment(&thread_base_index);
-//    //OMPTAutomatonState state;
-//    //// ompt_state_t allowed_states [] = {ompt_state_wait_critical};
-//    //// check_state(allowed_states, 1);
-//    //state.last_event = ompt_event_wait_critical;
-//    
-//    //pthread_mutex_lock(&map_mutex);
-//    //threadId_to_state[thread_index] = &state;
-//    //pthread_mutex_unlock(&map_mutex);
-//}
-//
-//void on_acquired_critical(ompt_wait_id_t wait_id)
-//{
-//    //long thread_index = fetch_and_increment(&thread_base_index);
-//    //OMPTAutomatonState * state = threadId_to_state[thread_index];
-//    //assert(state);
-//    //assert(state->last_event == ompt_event_wait_critical);
-//    
-//    // number of critical section entries should be odd
-//    //assert(state->num_critical_entries % 2 == 1);
-//    //state->last_event = ompt_event_acquired_critical;
-//}
-//
-void init_test(ompt_function_lookup_t lookup) {
-    //pthread_mutex_init(&map_mutex, NULL);
-    //REG_CB(wait_critical);
-    //REG_CB(acquired_critical);
-}
-//
-//void critical_section() {
-//    int a = 1;
-//    a++;
-//}
+using namespace std;
+pthread_mutex_t mutex;
 
-int main()
+volatile int waited = 0;
+volatile int acquired = 0;
+
+void on_wait_critical(ompt_wait_id_t wait_id)
 {
-    printf("\n>>> Test Critical: start\n");
-    
-    //#pragma omp parallel num_threads(NUM_THREADS)
-    //{
-    //    #pragma omp critical
-    //    {
-    //        critical_section();
-    //    }
-    //}
-    //
-    //for (long i = 0; i < NUM_THREADS; i++) {
-    //    OMPTAutomatonState * state = threadId_to_state[i];
-    //    assert(state);
-    //    assert(state->last_event == ompt_event_acquired_critical);
-    //    assert(state->num_critical_entries % 2 == 0);
-    //}
-    //
-    //printf("  Test Critical: success\n");
+  #pragma omp atomic update
+  waited += 1;
+}
+
+void on_acquired_critical(ompt_wait_id_t wait_id)
+{
+  #pragma omp atomic update
+  acquired += 1;
+}
+
+void init_test(ompt_function_lookup_t lookup) 
+{
+    if (!register_callback(ompt_event_wait_critical, (ompt_callback_t) on_wait_critical)) {
+        CHECK(FALSE, FATAL, "failed to register ompt_event_wait_critical");
+    }
+
+    if (!register_callback(ompt_event_acquired_critical, (ompt_callback_t) on_acquired_critical)) {
+        CHECK(FALSE, FATAL, "failed to register ompt_event_acquired_critical");
+    }
+}
+
+int main(int argc, char **argv)
+{
+    register_segv_handler(argv);
+
+    int max_threads = omp_get_max_threads();
+    int expected_waiters = max_threads - 1;
+
+    #pragma omp parallel num_threads(max_threads)
+    {
+	int myid = omp_get_thread_num();
+
+	// delay everyone but the master
+	if (myid != 0) {
+	   sleep(2);
+	}
+
+        #pragma omp critical
+        {
+            #pragma omp master
+            {
+              // master waits longer then everyone else, so they try to enter 
+              // the critical section while the master still is in it.
+              // that should cause all other workers to receive event 
+              // ompt_event_wait_critical. 
+              sleep(10);
+            }
+        }
+    }
+
+    CHECK(waited == expected_waiters, IMPLEMENTED_BUT_INCORRECT, "only %d of expected %d threads waited to enter a critical section", waited, expected_waiters);
+    CHECK(acquired == max_threads, IMPLEMENTED_BUT_INCORRECT, "only %d of %d expected threads acquired a critical section", acquired, max_threads);
 }

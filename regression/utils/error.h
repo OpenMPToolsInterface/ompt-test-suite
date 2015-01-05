c2c183a6305d6c3d69c993ebbe3405be5d1a05d7
John Mellor-Crummey <jmellorcrummey@comcast.net>
2015-01-04 19:54:37 -0600

    remove unnecessary include path in Makefile.intelomp. use _exit instead of exit for fatal errors.

--- regression/utils/error.h
+++ regression/utils/error.h
@@ -14,7 +14,7 @@
     pthread_mutex_lock(&assert_mutex); \
     printf("  %s: error at %s:%d",  executable_name, __FILE__, __LINE__); printf(" -- "  __VA_ARGS__ ); printf("\n"); \
     pthread_mutex_unlock(&assert_mutex); \
-    if (__error_code == FATAL) exit(FATAL); \
+    if (__error_code == FATAL) _exit(FATAL); \
     global_error_code = MIN(global_error_code, __error_code); }
 
 #define FATAL -3

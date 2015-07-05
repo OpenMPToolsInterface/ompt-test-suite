c6e8ddfa792ca23910447c69210d9d738b6897f1
Mellor-Crummey <johnmc@biou2.rice.edu>
2015-07-05 17:43:10 -0500

    slim down initialize test to work with new test in harness.

--- regression/mandatory/init/test_initialize.cpp
+++ regression/mandatory/init/test_initialize.cpp
@@ -32,14 +32,6 @@
 
 
 //*****************************************************************************
-// global variables
-//*****************************************************************************
-
-int init_success = 0;
-
-
-
-//*****************************************************************************
 // interface operations
 //*****************************************************************************
 
@@ -51,7 +43,7 @@ int ompt_initialize(void)
 #if DEBUG
   printf("ompt_initialize(void) called\n");
 #endif
-  init_success = 1;
+  ompt_initialized = 1;
   return 1;
 }
 
@@ -66,7 +58,7 @@ int ompt_initialize(ompt_function_lookup_t lookup,
   printf("ompt_initialize(lookup = %p, runtime_version = %s, "
          "ompt_version = %d)\n", runtime_version, ompt_version);
 #endif
-  init_success = 1;
+  ompt_initialized = 1;
   return 1;
 }
 
@@ -78,12 +70,7 @@ int ompt_initialize(ompt_function_lookup_t lookup,
 int
 regression_test(int argc, char **argv)
 {
-  int max_threads = omp_get_max_threads();
-
-  CHECK((max_threads > 0), IMPLEMENTED_BUT_INCORRECT, \
-	"ompt_get_max_threads() returned %d <= 0\n", max_threads);
-
-  CHECK(init_success, NOT_IMPLEMENTED, "ompt_initialize was not called\n");
+  // harness will guarantee that ompt_initialized has been set.
 
   return return_code;
 }
